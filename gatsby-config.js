0bf7590da4bab0726ddae0c76f5e13cf87b1cffd
Nalfs <nadim.al.sharif@gmail.com>
2019-03-12 14:51:57 +0100

    hello

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
     plugins: [
-         `gatsby-plugin-netlify-cms`,
-    {
+      `gatsby-plugin-netlify-cms`,
+      {
         resolve: `gatsby-source-filesystem`,
         options: {
           path: `${__dirname}/blog`,
-          name: "markdown-pages",
+          name: 'markdown-pages',
         },
       },
       `gatsby-transformer-remark`,
