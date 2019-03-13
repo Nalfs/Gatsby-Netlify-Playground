5b0ac9ebb1b51a69902ccfa815093b34771bd1ba
Nalfs <nadim.al.sharif@gmail.com>
2019-03-13 15:17:47 +0100

    change name on file

--- src/pages/contactForm.js
+++ src/pages/contactForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Contact from '../components/contact'
 import Header from '../components/header'
-import Footer from '../components/Footer'
+import Footer from '../components/footer'
 
 class ContactForm extends Component {
     render() {
